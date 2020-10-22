b0d3e5d8374dba0a7c6e480025f15d6d4200d74a
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-10-22 10:43:30 +0100

    pulled in API

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
 <div>
   <h1>Brewdog List</h1>
+
 </div>
   
 </template>
 
 <script>
 export default {
+  name:'App',
+  data() {
+    return {
+      beers:[]
+    }
+  },
+  mounted(){
+    this.getBeers()
+  },
 
-}
+  methods:{
+    getBeers:function() {
+      fetch('https://api.punkapi.com/v2/beers/')
+      .then(res => res.json())
+      .then(beers => this.beers = beers)
+    }
+    }
+  }
+
 </script>
 
 <style>
