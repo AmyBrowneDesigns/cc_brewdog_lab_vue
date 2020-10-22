a342a61d7b4b8b73ef0127796b96fab8f8b85fea
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-10-22 12:07:06 +0100

    added favourites button and list

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,33 @@
   <h1>Brewdog List</h1> 
   <label for="select_beer">Select a Beer</label>
   <select id="select_beer" v-model='selectedBeer'>
-    <option disabled value="">Select a Beer</option>
+    <option disabled selected value="">Select a Beer</option>
     <option v-for="beer in beers" :key="beer.id" :value="beer">{{beer.name}}</option>
   </select>
+
+<beer-detail v-if="selectedBeer" :selectedBeer="selectedBeer"></beer-detail>
+
+<button v-if="!favouriteBeers.includes(selectedBeer)" v-on:click="addToFavourites">Add beer to your favourites!
+</button>
+
+<favourite-beers :favouriteBeers="favouriteBeers">
+  </favourite-beers>
 
 </div>
   
 </template>
 
 <script>
+import BeerDetail from './components/BeerDetail.vue';
+import FavouriteBeer from './components/FavouriteBeer.vue';
+
 export default {
   name:'App',
   data() {
     return {
       beers:[],
-      selectedBeer:null
+      selectedBeer:null,
+      favouriteBeers: []
     }
   },
   mounted(){
@@ -29,12 +41,24 @@ export default {
       fetch('https://api.punkapi.com/v2/beers/')
       .then(res => res.json())
       .then(beers => this.beers = beers)
+    },
+    addToFavourites: function() {
+      this.favouriteBeers.push(this.selectedBeer)
     }
-    }
+  },
+
+  components: {
+    'beer-detail': BeerDetail,
+    'favourite-beers': FavouriteBeer
+  }
+
   }
 
 </script>
 
 <style>
+.small-beer {
+  height: 80px;
+}
 
 </style>
---
+++ src/components/BeerDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="selected_beer">
+        <h2>Beer name: {{ selectedBeer.name }}</h2>  
+        <p>Tagline: {{ selectedBeer.tagline }}</p>
+        <p>Description: {{ selectedBeer.description}}</p>
+        <p>ABV: {{ selectedBeer.abv }}</p>
+        <img class="beer-image" v-if="selectedBeer" :src="selectedBeer.image_url" alt="image_of_bottle" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'beer-detail',
+    props: ['selectedBeer']
+}
+</script>
+
+<style>
+    .beer-image {
+        max-height: 250px;
+    }
+</style>
